f34c30203b26069736ebbdc81beda46db3dca393
Bang SungWon <bsw9698@gmail.com>
2022-09-08 12:02:30 +0900

    스와이퍼 레이아웃 추가

---
+++ lib/Content.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+class Content extends StatelessWidget {
+  const Content({Key? key, this.listKeyword, this.listOption, this.removeKeyword, this.index}) : super(key: key);
+  final listKeyword;
+  final listOption;
+  final removeKeyword;
+  final index;
+
+
+  @override
+  Widget build(BuildContext context) {
+    print(listKeyword);
+    return Padding(
+      padding: EdgeInsets.all(12.0),
+      child: Container(
+        height: 200,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(
+              Radius.circular(12.0),
+            ),
+            boxShadow : [
+              BoxShadow(
+                  spreadRadius: 2,
+                  blurRadius: 1,
+                  color: Color(0xD3CFCFFF)
+              )
+            ]
+        ),
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.fromLTRB(20, 5, 20, 5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text("${listKeyword[index]}    - ${listOption[index]} -", style: TextStyle(
+                    fontSize: 20
+                  ),),
+                  IconButton(
+                      onPressed: (){
+                        removeKeyword(listKeyword[index], listOption[index]);
+                      },
+                      icon: FaIcon(FontAwesomeIcons.trash)
+                  )
+                ],
+              ),
+            ),
+            Flexible(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(20, 5, 10, 5),
+                child: Swiper(
+                  itemBuilder: (BuildContext context, int index) {
+                    return Image.asset(
+                      'assets/images/cat.gif',
+                      fit: BoxFit.fill,
+                    );
+                  },
+                  autoplay: true,
+                  itemCount: 3,
+                  pagination: SwiperPagination(),
+                  control: SwiperControl(),
+                )
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,12 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:stock_manager/drawer.dart';
 import './style.dart' as style;
 import 'package:sizer/sizer.dart';
+import './Content.dart';
 
 List<DropdownMenuItem<String>> get dropdownItems{
   List<DropdownMenuItem<String>> menuItems = [
-    DropdownMenuItem(child: Text("None"),value: "None"),
-    DropdownMenuItem(child: Text("Video"),value: "Video"),
     DropdownMenuItem(child: Text("Article"),value: "Article"),
+    DropdownMenuItem(child: Text("Video"),value: "Video"),
     DropdownMenuItem(child: Text("Img"),value: "Img"),
   ];
   return menuItems;
@@ -25,17 +25,44 @@ void main() {
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
-
-
-
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
   var num = 0;
-  var selectedNum = 'None';
+  var selected = 'Article';
   var scroll = ScrollController();
+  var listCount = 0;
+  var listKeyword = [];
+  var listOption = [];
+  var keyword;
+
+
+  Keyword(e){
+    setState(() {
+      keyword = e;
+    });
+  }
+
+
+  addKeyword(){
+    setState(() {
+      listKeyword.add(keyword);
+      listOption.add(selected);
+      listCount += 1;
+    });
+  }
+
+
+
+  removeKeyword(keyword, option){
+    setState(() {
+      listKeyword.remove(keyword);
+      listOption.remove(option);
+      listCount -= 1;
+    });
+  }
 
   @override
   void initState() {
@@ -45,43 +72,45 @@ class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text('StockManager'),
-        actions: [
-          TextButton(
-              onPressed: (){
-              },
-              child: FaIcon(FontAwesomeIcons.plus)
-          ),
-
-        ]
+        // actions: [
+        //   TextButton(
+        //       onPressed: (){
+        //       },
+        //       child: FaIcon(FontAwesomeIcons.plus)
+        //   ),
+        //
+        // ]
       ),
       body:Container(
         child: Column(
           children: [
             Padding(
-              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 16),
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
               child: Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Flexible(
                     flex: 8,
                     child: TextFormField(
+                      onChanged: (String? e){
+                        Keyword(e);
+                      },
                       decoration: InputDecoration(
                         labelText: 'Enter Keyword',
                       ),
                     ),
                   ),
+                  Spacer(),
                   Flexible(
                     flex: 4,
                       child: DropdownButton(
-                        value: selectedNum,
-                        onChanged: (String? e){
-                          setState(() {
-                            selectedNum = e!;
-                          });
-                        },
+                        value: selected,
+                        onChanged: (String? e){},
                         items: dropdownItems,
                       )
                   ),
@@ -90,37 +119,30 @@ class _MyAppState extends State<MyApp> {
                     child: TextButton(
                         style: TextButton.styleFrom(
                             fixedSize: Size.fromHeight(60),
-                            shape: RoundedRectangleBorder(
-                          )
                         ),
-                        onPressed: (){
-
+                        onPressed: () {
+                          if(keyword != '' && keyword != null){
+                            addKeyword();
+                          }
                         },
                         child: Align(
                           alignment: Alignment.center,
-                          child: Icon(Icons.search, size: 40),
-                        )
-                    )
-                  )
-                ],
-              )
-            ),
+                          child: Icon(Icons.search, size: 40)
+                        ),
+                      )
+                    ),
+                  ],
+                ),
+              ),
             Expanded(
               child: ListView.builder(
-                controller: scroll,
-                shrinkWrap: true,
-                itemCount: 10,
-                itemBuilder: (context, index){
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      height: 100,
-                      decoration: BoxDecoration(
-                        color: Colors.green,
-                      ),
-                    ),
-                  );
-                }
+                  controller: scroll,
+                  shrinkWrap: true,
+                  itemCount: listCount,
+                  itemBuilder: (context, index){
+                    return Content(listKeyword : listKeyword, listOption : listOption,
+                      removeKeyword: removeKeyword, index: index);
+                  }
               ),
             )
           ],
@@ -130,5 +152,8 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
+
+
+
 
 
